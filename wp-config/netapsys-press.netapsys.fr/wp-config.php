ac79373d21ca7b52b628ef7d266da9782b703bba
johary <johary.ranarimanana@netapsys.fr>
2016-04-28 19:14:10 +0300

    config prod

--- wp-config/netapsys-press.netapsys.fr/wp-config.php
+++ wp-config/netapsys-press.netapsys.fr/wp-config.php
@@ -22,10 +22,10 @@
 define('DB_NAME', 'netapsys-press');
 
 /** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'root');
+define('DB_USER', 'dev');
 
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'D7JoT6MwWwdWUP5vX91h');
 
 /** Adresse de l'hébergement MySQL. */
 define('DB_HOST', 'localhost');
