a8e73d6843c1426d2cd21ae1faab4a444b8c39fc
johary <johary.ranarimanana@netapsys.fr>
2016-06-23 17:44:26 +0300

    maj acf collpaser

--- wp-content/plugins/acf-repeater-collapser-admin/js/acf-repeater-collapser-admin.js
+++ wp-content/plugins/acf-repeater-collapser-admin/js/acf-repeater-collapser-admin.js
@@ -1,33 +1,21 @@
 jQuery(document).ready(function($) {
-
-    $('.field_type-repeater').each( function() {
-        if( $( '.acf-input-table', $(this) ).hasClass('row_layout') ) {
-            $(this).find('.acf_input tr').eq(0).addClass('first_tr');
+    $('.field_type-repeater .repeater .acf-input-table.row_layout > tbody > tr.row > .acf_input-wrap > .acf_input').each(function(){
+        _this = $(this);
+        _this.addClass('collapsed-repeater');
+        var _value = _this.find('input[type=text]').eq(0).val();
+        _value = _value.substring(0,20);
+        if ( _value ) {
+            _this.find('> tbody > tr:first-child > td.label > label').append('<em class="collapse-value">' + '(' + _value + '...)' + '</em>' );
         }
-    });
-
-    $('.repeater-footer .add-row-end.acf-button').live("click", function(){
-        $(this).parents('.acf-input-table').find('tr.row').each(function() {
-            $(this).find('.acf_input tr').eq(0).addClass('first_tr');
-        });
-        var $repeate_div = $(this).parents('.repeater');
-        setTimeout(function(){
-            $repeate_div.find('.row_layout tr.row').each(function() {
-                $(this).find('.acf_input tr').eq(0).addClass('first_tr');
-            });
-        },500);
-    });
-
-    // Hide all but the first Rows
-    $(".field_type-repeater .repeater .row .acf_input-wrap table.acf_input").addClass('collapsed-repeater');
+    })
 
     // Show/hide all rows if label gets clicked
-    $(".field_type-repeater .repeater .row .acf_input-wrap table.acf_input .label").live("click", function(){
+    $(".field_type-repeater .repeater .acf-input-table.row_layout > tbody > tr.row > .acf_input-wrap > .acf_input > tbody > tr:first-child > td.label").live("click", function(){
         _this = $(this);
         _parent =  $(this).parents('.acf_input').eq(0);
         _parent.toggleClass('collapsed-repeater');
         if ( _parent.hasClass('collapsed-repeater') ) {
-            var _value = _this.parents('.acf_input').eq(0).find('input[type=text]').val();
+            var _value = _parent.find('input[type=text]').eq(0).val();
             _value = _value.substring(0,20);
             if ( _value ) {
                 _this.find('label').append('<em class="collapse-value">' + '(' + _value + '...)' + '</em>' );
