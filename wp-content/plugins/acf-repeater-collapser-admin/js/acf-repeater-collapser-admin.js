d59f12ea0a527f35c8f3390c667d479c82b91263
johary <johary.ranarimanana@netapsys.fr>
2016-06-24 08:21:54 +0300

    js collapser

--- wp-content/plugins/acf-repeater-collapser-admin/js/acf-repeater-collapser-admin.js
+++ wp-content/plugins/acf-repeater-collapser-admin/js/acf-repeater-collapser-admin.js
@@ -2,9 +2,9 @@ jQuery(document).ready(function($) {
     $('.field_type-repeater .repeater .acf-input-table.row_layout > tbody > tr.row > .acf_input-wrap > .acf_input').each(function(){
         _this = $(this);
         _this.addClass('collapsed-repeater');
-        var _value = _this.find('input[type=text]').eq(0).val();
-        _value = _value.substring(0,20);
+        var _value = _this.find('input[type=text][value!=""]').eq(0).val();
         if ( _value ) {
+            _value = _value.substring(0,20);
             _this.find('> tbody > tr:first-child > td.label > label').append('<em class="collapse-value">' + '(' + _value + '...)' + '</em>' );
         }
     })
@@ -13,12 +13,12 @@ jQuery(document).ready(function($) {
     $(".field_type-repeater .repeater .acf-input-table.row_layout > tbody > tr.row > .acf_input-wrap > .acf_input > tbody > tr:first-child > td.label").live("click", function(){
         _this = $(this);
         _parent =  $(this).parents('.acf_input').eq(0);
+        var _value = _parent.find('input[type=text][value!=""]:visible').eq(0).val();
         _parent.toggleClass('collapsed-repeater');
         if ( _parent.hasClass('collapsed-repeater') ) {
-            var _value = _parent.find('input[type=text]').eq(0).val();
-            _value = _value.substring(0,20);
             if ( _value ) {
-                _this.find('label').append('<em class="collapse-value">' + '(' + _value + '...)' + '</em>' );
+                _value = _value.substring(0,20);
+                _this.find('>label').append('<em class="collapse-value">' + '(' + _value + '...)' + '</em>' );
             }
         } else {
             _this.find('.collapse-value').remove();
