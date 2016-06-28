739fb19650eed4501570d2475f035b20668d2b16
johary <johary.ranarimanana@netapsys.fr>
2016-06-29 00:27:19 +0300

    acf collapser livequery

--- wp-content/plugins/acf-repeater-collapser-admin/acf-repeater-collapser-admin.php
+++ wp-content/plugins/acf-repeater-collapser-admin/acf-repeater-collapser-admin.php
@@ -8,15 +8,20 @@
  */
 
 /* Load the javascript and CSS files on the ACF admin pages */
-add_action( 'admin_enqueue_scripts', 'carambole_repeater_collapser_assets' );
-function carambole_repeater_collapser_assets() {
+add_action( 'admin_enqueue_scripts', 'acf_repeater_collapser_assets' );
+function acf_repeater_collapser_assets() {
 	wp_enqueue_script(
-		'carambole_repeater_collapser_admin_js',
+		'acf_repeater_collapser_admin_js',
     plugin_dir_url( __FILE__ ) . 'js/acf-repeater-collapser-admin.js',
 		array( 'jquery' )
 	);
+  wp_enqueue_script(
+    'acf_repeater_collapser_livequery',
+    plugin_dir_url( __FILE__ ) . 'js/jquery.livequery.js',
+    array( 'jquery' )
+  );
 	wp_enqueue_style(
-		'carambole_repeater_collapser_admin_css',
+		'acf_repeater_collapser_admin_css',
     plugin_dir_url( __FILE__ ) . 'css/acf-repeater-collapser-admin.css'
 	);
 }
